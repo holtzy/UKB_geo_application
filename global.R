5771f1b7bfaf94c715f0b96aa2436870b162a8b3
Holtz Yan <yan.holtz.data@gmail.com>
2018-03-14 15:35:56 +1000

    Automatize libraries installation

--- global.R
+++ global.R
@@ -3,24 +3,53 @@
 # -----------------------------------
 # 1- LOAD LIBRARIES
 
-# libraries
-library(shiny)
-library(dplyr)
-library(tibble)
-library(ggplot2)
-library(tidyr)
-library(shinyWidgets)
-library(RColorBrewer)		# awesome color palettes
-library(d3heatmap)			# for heatmaps
-library(plotly)
-library(DT)
-library(viridis)
-library(shinycssloaders)	# Spinner if plot takes time to load
-library(sp)
-library(rgdal)
-library(rgeos)
-library(leaflet)
-library(d3heatmap)
+# A function that install packages if not available/installed.
+install_missing_packages = function(pkg, version = NULL, verbose = TRUE){
+  availpacks = .packages(all.available = TRUE)
+  missingPackage = FALSE
+  if(!any(pkg %in% availpacks)){
+    if(verbose){
+      message("The following package is missing.\n",
+              pkg, "\n",
+              "Installation will be attempted...")
+    }
+    missingPackage <- TRUE
+  }
+  if(!is.null(version) & !missingPackage){
+    # version provided and package not missing, so compare.
+    if( compareVersion(a = as.character(packageVersion(pkg)),
+                       b = version) < 0 ){
+      if(verbose){
+        message("Current version of package\n", 
+                pkg, "\t", 
+                packageVersion(pkg), "\n",
+                "is less than required.
+                Update will be attempted.")
+      }
+      missingPackage <- TRUE
+    }
+  }
+  if(missingPackage){
+    print(pkg)
+    print(paste("---- installing a more recent version of",pkg,sep=""))
+	install.packages(pkg, repos = "http://cran.r-project.org")  }
+}
+
+
+# Define list of package names and required versions.
+deppkgs = c(shiny="0.14.2", dplyr="0.7.4", tibble="1.4.2", ggplot2 = "2.2.1.9", tidyr="0.7.2", shinyWidgets="0.4.1", 
+			RColorBrewer="1.1.2", plotly = "4.5.6", DT="0.2", viridis="0.4.0", shinycssloaders="0.2.0", sp="1.2",
+			rgdal="1.2", rgeos="0.3", leaflet="1.1.0", stringr="1.3.0", maps="3.2.0", mapdata="2.2", lattice="0.20",
+			spdep="0.7", cartogram="0.0.2")
+
+# Loop on package: check, install, update
+pkg1 = mapply(install_missing_packages,
+              pkg = names(deppkgs), 
+              version = deppkgs,
+              MoreArgs = list(verbose = TRUE), 
+              SIMPLIFY = FALSE,
+              USE.NAMES = TRUE)
+          
 
 
 
